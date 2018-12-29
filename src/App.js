6cd039cff19341a04e80105c5b80e8abef8cbee8
Evaldas Suraucius <surauciusevaldas@gmail.com>
2018-12-29 14:57:47 +0200

    Added action, reducer, store. Refactored some code to use Redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
 import './App.css'
+import {createStore} from 'redux';
 
 
+// our constant
+const CHANGE_NAME = 'CHANGE_NAME';
+
+// action creator
+const changeName = (name) => {
+  return {
+    type: CHANGE_NAME,
+    payload: {
+      name
+    }
+  }
+}
+
+// reducer
+const nameReducer = (state = 'World', action) => {
+  switch (action.type){
+    case CHANGE_NAME:
+      return {
+        name: action.payload.name
+      };
+
+    default:
+      return state;
+  }
+}
+
+// initial state
+const initialState = {
+  name: 'World!'
+}
+
+// store
+const store = createStore(nameReducer, initialState);
 
 class App extends Component {
 
   constructor(props){
     super(props);
-
     this.state = {
-      name: 'World',
       input: ''
-    }
+    }    
   }
 
   handleChange = (event) => {
@@ -21,20 +53,22 @@ class App extends Component {
   }
 
   submitName = () => {
+    // before reseting input, dispatch an action to change name
+    store.dispatch(changeName(this.state.input));
+
     this.setState({
-      name: this.state.input,
       input: ''
-    })
+    });
   }
 
   render() {
     return (
       <div className="App">
-      <img src={require('./minions.jpg')} alt='minions.jpg' title='happy minions' />
+      <img src={require('./images/minions.jpg')} alt='minions.jpg' title='happy minions' />
         <br />
         <input value={this.state.input} onChange={this.handleChange} />
-        <button onClick={this.submitName} >Change your name</button>
-        <p>Hello {this.state.name}!</p>
+        <button onClick={this.submitName} >Change greeting name</button>
+        <p>Hello {store.getState().name}!</p>
       </div>
     );
   }
