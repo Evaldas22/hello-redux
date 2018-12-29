2290dad32ad9d8cb01549b9dfa184189e9ddab7b
Evaldas Suraucius <surauciusevaldas@gmail.com>
2018-12-29 13:47:05 +0200

    Added functionality to change name and display it

--- src/App.js
+++ src/App.js
@@ -5,10 +5,36 @@ import './App.css'
 
 class App extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      name: 'World',
+      input: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  submitName = () => {
+    this.setState({
+      name: this.state.input,
+      input: ''
+    })
+  }
+
   render() {
     return (
       <div className="App">
-
+      <img src={require('./minions.jpg')} alt='minions.jpg' title='happy minions' />
+        <br />
+        <input value={this.state.input} onChange={this.handleChange} />
+        <button onClick={this.submitName} >Change your name</button>
+        <p>Hello {this.state.name}!</p>
       </div>
     );
   }
